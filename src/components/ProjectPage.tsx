526c161a27e206394520e12603ce620dabfca9de
Gunn Jain <gunn.jain2005@gmail.com>
2026-01-15 19:50:01 +0530

    hey

--- src/components/ProjectPage.tsx
+++ src/components/ProjectPage.tsx
@@ -105,7 +105,7 @@ export default function VillaProjectPage({
           <div
             key={group.id}
             ref={(el) => (mobileRefs.current[index] = el)}
-            className="mb-[90vh]"
+            className="mb-[50vh]"
           >
             <h2 className="text-[26px] font-light leading-tight mb-3">
               {group.title}
